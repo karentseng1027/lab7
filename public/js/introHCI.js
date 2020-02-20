7b49e96e7596c978b1788bdb708f6fd3bd796e63
104062212 <karentseng1027@gmail.com>
2020-02-20 08:59:26 -0800

    /viewAlt added, like button connected to googleAlt

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(sendAlt);
+}
+
+function sendAlt(e) {
+	e.preventDefault();
+	console.log("click");
+	ga("send", "event", "like", "click");
 }
